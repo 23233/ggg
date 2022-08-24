19807b04bbaa8ce308167b6f4b92d006a90ac7f5
1 <1@qq.com>
2022-08-24 21:32:10 +0800

    `sv` 使用iris自带的read方法

--- sv/main.go
+++ sv/main.go
@@ -3,7 +3,6 @@ package sv
 import (
 	"github.com/kataras/iris/v12"
 	"reflect"
-	"strings"
 )
 
 var (
@@ -15,59 +14,26 @@ var (
 	GlobalContextKey = "sv"
 )
 
-func Run(valid interface{}, mode ...string) iris.Handler {
-	var m string
-	if len(mode) >= 1 {
-		m = mode[0]
-	}
-
+func Run(valid interface{}) iris.Handler {
 	return func(ctx iris.Context) {
 		ctx.RecordRequestBody(true)
 		// 回复到初始状态
 		s := reflect.TypeOf(valid).Elem()
 		newS := reflect.New(s)
 		v := newS.Interface()
-		var err error
-		switch m {
-		case "query":
-			err = ctx.ReadQuery(v)
-			break
-		case "json":
-			err = ctx.ReadJSON(v)
-			break
-		case "xml":
-			err = ctx.ReadXML(v)
-			break
-		case "form":
-			err = ctx.ReadForm(v)
-			break
-		default:
-			if ctx.Method() == "GET" {
-				err = ctx.ReadQuery(v)
-			} else {
-				contentType := ctx.GetContentTypeRequested()
-				if contentType == "application/x-www-form-urlencoded" || strings.HasPrefix(contentType, "multipart/form-data") {
-					err = ctx.ReadForm(v)
-				} else if contentType == "application/xml" {
-					err = ctx.ReadXML(v)
-				} else {
-					err = ctx.ReadJSON(v)
-				}
-			}
-			break
-		}
+		err := ctx.ReadBody(v)
 		if err != nil {
 			Warning.Printf("read valid data fail: %s", err.Error())
 			GlobalFailFunc(err, ctx)
 			return
 		}
 
-		if err := GlobalValidator.Check(v); err != nil {
+		if err = GlobalValidator.Check(v); err != nil {
 			Warning.Printf("valid fields fail: %s", err.Error())
 			GlobalFailFunc(err, ctx)
 			return
 		}
-		// this is point structv
+		// this is point struct
 		ctx.Values().Set(GlobalContextKey, v)
 		ctx.Next()
 	}

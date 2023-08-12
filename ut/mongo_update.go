60b49cfa31f8536a67f889426c467a6136333bca
1 <1@qq.com>
2023-08-12 15:38:45 +0800

    update `ut`

--- ut/mongo_update.go
+++ ut/mongo_update.go
@@ -1,11 +1,24 @@
 package ut
 
 import (
+	"archive/zip"
+	"encoding/csv"
+	"encoding/json"
+	"encoding/xml"
 	"github.com/google/go-cmp/cmp"
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"image"
+	"math/big"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
 	"reflect"
+	"regexp"
 	"strings"
+	"time"
 )
 
 // StructConverter 作用在于struct中如果需要更新的时候 不要使用bson.M来手动设置字段名
@@ -15,8 +28,44 @@ import (
 // 这时候就使用 diff 的模式 对比两个struct中的不同项进行变更
 type StructConverter struct {
 	CustomIsZero map[reflect.Type]func(reflect.Value, reflect.StructField) bool
+	SpecialTypes []reflect.Type // 特殊结构体类型列表
 }
 
+func (sc *StructConverter) InitSpecialTypes() {
+	sc.SpecialTypes = []reflect.Type{
+		reflect.TypeOf(time.Time{}),
+		reflect.TypeOf(net.IP{}),
+		reflect.TypeOf(net.IPNet{}),
+		reflect.TypeOf(url.URL{}),
+		reflect.TypeOf(regexp.Regexp{}),
+		reflect.TypeOf(os.File{}),
+		reflect.TypeOf((*os.FileInfo)(nil)).Elem(),
+		reflect.TypeOf(big.Int{}),
+		reflect.TypeOf(big.Float{}),
+		reflect.TypeOf(big.Rat{}),
+		reflect.TypeOf(zip.Reader{}),
+		reflect.TypeOf(zip.Writer{}),
+		reflect.TypeOf(http.Request{}),
+		reflect.TypeOf(http.Response{}),
+		reflect.TypeOf((*image.Image)(nil)).Elem(),
+		reflect.TypeOf(csv.Reader{}),
+		reflect.TypeOf(csv.Writer{}),
+		reflect.TypeOf(json.Decoder{}),
+		reflect.TypeOf(json.Encoder{}),
+		reflect.TypeOf(xml.Decoder{}),
+		reflect.TypeOf(xml.Encoder{}),
+	}
+}
+
+func (sc *StructConverter) isSpecialStructType(t reflect.Type) bool {
+	// 检查给定类型是否在特殊类型列表中
+	for _, specialType := range sc.SpecialTypes {
+		if t == specialType {
+			return true
+		}
+	}
+	return false
+}
 func (sc *StructConverter) IsZero(v reflect.Value, field reflect.StructField) bool {
 	if customIsZero, ok := sc.CustomIsZero[v.Type()]; ok && customIsZero(v, field) {
 		return true
@@ -62,6 +111,10 @@ func (sc *StructConverter) structToMap(val reflect.Value, freezeMap map[string]b
 	for i := 0; i < val.NumField(); i++ {
 		fieldVal := val.Field(i)
 		fieldType := typ.Field(i)
+		// 如果字段未导出，则跳过
+		if !fieldType.IsExported() {
+			continue
+		}
 
 		if sc.IsZero(fieldVal, fieldType) {
 			continue
@@ -75,20 +128,14 @@ func (sc *StructConverter) structToMap(val reflect.Value, freezeMap map[string]b
 		bsonTagParts := strings.Split(bsonTag, ",")
 		bsonTag = bsonTagParts[0]
 
-		// 有inline则为内联
-		// 没有名称也视为内联
-
-		isInline := false
-		if fieldVal.Kind() == reflect.Struct || fieldVal.Kind() == reflect.Map {
-			if strings.Contains(bsonTag, "inline") {
-				isInline = true
-			} else if bsonTag == "" {
-				isInline = true
-			}
+		isInline := strings.Contains(bsonTag, "inline") || (bsonTag == "" && fieldType.Anonymous)
+		if isInline {
+			bsonTag = strings.TrimSuffix(bsonTag, ",inline")
 		}
 
-		if !isInline && bsonTag == "" {
-			bsonTag = fieldType.Name // 使用字段名称作为键，如果没有Bson标签
+		// 如果bsonTag未定义且不是内联，则使用字段名
+		if bsonTag == "" && !isInline {
+			bsonTag = fieldType.Name
 		}
 
 		fullPath := prefix + bsonTag
@@ -97,33 +144,32 @@ func (sc *StructConverter) structToMap(val reflect.Value, freezeMap map[string]b
 			continue
 		}
 
-		if isInline {
-			if fieldVal.Kind() == reflect.Struct {
-				inlinePrefix := prefix
-				if bsonTag != "" {
-					inlinePrefix += bsonTag + "."
-				}
-				inlineMap := sc.structToMap(fieldVal, freezeMap, inlinePrefix)
-				for k, v := range inlineMap {
-					result[k] = v
-				}
-				continue
-			}
+		if sc.isSpecialStructType(fieldVal.Type()) {
+			// 如果字段是特殊结构体类型，则直接返回值
+			result[fullPath] = fieldVal.Interface()
+			continue
 		}
 
-		if fieldVal.Kind() == reflect.Struct {
-			nestedMap := sc.structToMap(fieldVal, freezeMap, fullPath+".")
-			if len(nestedMap) > 0 {
-				result[bsonTag] = nestedMap
+		if isInline || fieldVal.Kind() == reflect.Struct || fieldVal.Kind() == reflect.Map {
+			inlinePrefix := prefix
+			if !isInline {
+				inlinePrefix += bsonTag + "."
 			}
-		} else {
-			result[bsonTag] = fieldVal.Interface()
+			inlineMap := sc.structToMap(fieldVal, freezeMap, inlinePrefix)
+			for k, v := range inlineMap {
+				result[k] = v
+			}
+			continue
 		}
+
+		result[fullPath] = fieldVal.Interface()
 	}
 
 	return result
 }
 
+// DiffToBsonM 缺点在于如果有内联的struct 如果原始数据中没有定义上层 则无法正确update
+// 比如{city:{name:"新名称"}} 如果原始数据中没有 city字段则无法通过{city.name:"新名称"}进行更新?
 func (sc *StructConverter) DiffToBsonM(original, current interface{}, freeze ...string) (bson.M, error) {
 	originalVal := reflect.ValueOf(original)
 	currentVal := reflect.ValueOf(current)
@@ -131,7 +177,6 @@ func (sc *StructConverter) DiffToBsonM(original, current interface{}, freeze ...
 	if originalVal.Kind() == reflect.Ptr {
 		originalVal = originalVal.Elem()
 	}
-
 	if currentVal.Kind() == reflect.Ptr {
 		currentVal = currentVal.Elem()
 	}
@@ -154,25 +199,24 @@ func (sc *StructConverter) diffStructToMap(originalVal, currentVal reflect.Value
 	for i := 0; i < originalVal.NumField(); i++ {
 		fieldType := typ.Field(i)
 		bsonTag := fieldType.Tag.Get("bson")
-		if bsonTag == "-" {
+		if bsonTag == "-" || !fieldType.IsExported() {
 			continue
 		}
 
-		fieldVal := originalVal.Field(i)
-		isInline := false
-		if fieldVal.Kind() == reflect.Struct || fieldVal.Kind() == reflect.Map {
-			if strings.Contains(bsonTag, "inline") {
-				isInline = true
-			} else if bsonTag == "" {
-				isInline = true
-			}
+		isInline := strings.Contains(bsonTag, "inline") || (bsonTag == "" && fieldType.Anonymous)
+		if isInline {
+			bsonTag = strings.TrimSuffix(bsonTag, ",inline")
 		}
 
-		if !isInline && bsonTag == "" {
-			bsonTag = fieldType.Name // 使用字段名称作为键，如果没有Bson标签
+		// 如果bsonTag未定义且不是内联，则使用字段名
+		if bsonTag == "" && !isInline {
+			bsonTag = fieldType.Name
 		}
 
-		fullPath := prefix + bsonTag
+		fullPath := bsonTag
+		if prefix != "" && !isInline {
+			fullPath = prefix + bsonTag
+		}
 
 		if freezeMap[fullPath] {
 			continue
@@ -182,23 +226,21 @@ func (sc *StructConverter) diffStructToMap(originalVal, currentVal reflect.Value
 		currentFieldVal := currentVal.Field(i)
 
 		if isInline {
-			inlinePrefix := prefix
-			if bsonTag != "" {
-				inlinePrefix += bsonTag + "."
-			}
-			inlineMap := sc.diffStructToMap(originalFieldVal, currentFieldVal, freezeMap, inlinePrefix)
+			// 处理带有inline标签的匿名字段
+			inlineMap := sc.diffStructToMap(originalFieldVal, currentFieldVal, freezeMap, prefix)
 			for k, v := range inlineMap {
 				result[k] = v
 			}
-			continue
-		}
-
-		if !cmp.Equal(originalFieldVal.Interface(), currentFieldVal.Interface()) {
-			if currentFieldVal.Kind() == reflect.Struct {
-				result[bsonTag] = sc.diffStructToMap(originalFieldVal, currentFieldVal, freezeMap, fullPath+".")
-			} else {
-				result[bsonTag] = currentFieldVal.Interface()
+		} else if originalFieldVal.Kind() == reflect.Struct {
+			// 处理嵌套结构体
+			nestedPrefix := fullPath + "."
+			nestedMap := sc.diffStructToMap(originalFieldVal, currentFieldVal, freezeMap, nestedPrefix)
+			for k, v := range nestedMap {
+				result[k] = v
 			}
+		} else if !cmp.Equal(originalFieldVal.Interface(), currentFieldVal.Interface()) {
+			// 使用 cmp.Equal 比较字段值
+			result[fullPath] = currentFieldVal.Interface()
 		}
 	}
 
@@ -208,13 +250,35 @@ func (sc *StructConverter) diffStructToMap(originalVal, currentVal reflect.Value
 // ToUpdateBson struct更新内容给转换为bson.M freeze支持格式 a.b.c
 // 只会更新存在的 有值的 若原本有值要清除 请使用diff
 func ToUpdateBson(input any, freeze ...string) (bson.M, error) {
-	converter := new(StructConverter)
+	converter := NewStructConverter()
+	converter.AddCustomIsZero(new(primitive.ObjectID), func(v reflect.Value, field reflect.StructField) bool {
+		return v.Interface().(primitive.ObjectID).IsZero()
+	})
+	converter.AddCustomIsZero(new(time.Time), func(v reflect.Value, field reflect.StructField) bool {
+		return v.Interface().(time.Time).IsZero()
+	})
 	return converter.StructToBsonM(input, freeze...)
 }
 
 // ToDiffBson 两个struct找出不同项 支持freeze格式为 a.b.c
 // 只会对比值不同的 两个struct必须相同
 func ToDiffBson[T any](original, current T, freeze ...string) (bson.M, error) {
-	converter := new(StructConverter)
+	converter := NewStructConverter()
+
 	return converter.DiffToBsonM(original, current, freeze...)
+}
+
+func NewStructConverter() *StructConverter {
+	converter := &StructConverter{
+		SpecialTypes: []reflect.Type{
+			reflect.TypeOf(time.Time{}),
+		},
+	}
+	converter.AddCustomIsZero(new(primitive.ObjectID), func(v reflect.Value, field reflect.StructField) bool {
+		return v.Interface().(primitive.ObjectID).IsZero()
+	})
+	converter.AddCustomIsZero(new(time.Time), func(v reflect.Value, field reflect.StructField) bool {
+		return v.Interface().(time.Time).IsZero()
+	})
+	return converter
 }
--- ut/mongo_update_test.go
+++ ut/mongo_update_test.go
@@ -1,10 +1,12 @@
 package ut
 
 import (
+	"github.com/google/go-cmp/cmp"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type InnerOne struct {
@@ -37,11 +39,17 @@ type MainTypes struct {
 		Inline `bson:",inline"`
 		InnerAnonymous
 		InlineName `bson:"inline_name,inline"`
+		Tm         time.Time   `bson:"tm"`
+		SliceStr   []string    `bson:"slice_str"`
+		SliceTime  []time.Time `bson:"slice_time"`
 	} `bson:"anonymous"`
+	Tm        time.Time   `bson:"tm"`
+	SliceStr  []string    `bson:"slice_str"`
+	SliceTime []time.Time `bson:"slice_time"`
 }
 
 func TestStructToBsonM(t *testing.T) {
-	converter := &StructConverter{}
+	converter := NewStructConverter()
 	converter.AddCustomIsZero(new(primitive.ObjectID), func(v reflect.Value, field reflect.StructField) bool {
 		return v.Interface() == primitive.NilObjectID
 	})
@@ -69,6 +77,9 @@ func TestStructToBsonM(t *testing.T) {
 			Inline `bson:",inline"`
 			InnerAnonymous
 			InlineName `bson:"inline_name,inline"`
+			Tm         time.Time   `bson:"tm"`
+			SliceStr   []string    `bson:"slice_str"`
+			SliceTime  []time.Time `bson:"slice_time"`
 		}{
 			Inline: Inline{
 				Desc:  "anonymous description",
@@ -76,62 +87,86 @@ func TestStructToBsonM(t *testing.T) {
 			},
 			InnerAnonymous: InnerAnonymous{
 				Name:     "Anonymous Alice",
-				NickName: "AA",
+				NickName: "AAA",
 			},
 			InlineName: InlineName{
 				InName: "Anonymous Inner Name",
 				Desc:   "Anonymous Inner Description",
 			},
+			Tm:        time.Now(),
+			SliceStr:  []string{"222"},
+			SliceTime: []time.Time{time.Now()},
 		},
+		Tm:        time.Now(),
+		SliceStr:  []string{"111"},
+		SliceTime: []time.Time{time.Now()},
 	}
 
 	result, _ := converter.StructToBsonM(main)
 
-	// 验证主要字段
-	// 因为InlineName有inline标签 但是他排在Inline结构的后面有相同的desc字段 应该覆盖前面的desc字段
-	if result["desc"] != "Inner Description" || result["obj_id"] != objID {
-		t.Errorf("Unexpected values for inline fields")
-	}
-	if result["name"] != "Alice" || result["nick_name"] != "A" {
-		t.Errorf("Unexpected values for InnerAnonymous fields")
-	}
-	if result["avatar"] != "avatar.png" {
-		t.Errorf("Unexpected value for Avatar field")
-	}
-	if result["NoBsonTag"] != "no bson tag" {
-		t.Errorf("Unexpected value for NoBsonTag field")
-	}
-
-	// 因为inline_name有inline标签 他不应该出现
-	inlineName, ok := result["inline_name"].(bson.M)
-	if ok || inlineName != nil {
-		t.Errorf("inline标签解析失败 未提取到上一级中")
-	}
-
-	// 验证内部字段
-	innerOne, ok := result["inner_one"].(bson.M)
-	if !ok || innerOne["city"] != "New York" {
-		t.Errorf("Unexpected values for InnerOne fields")
+	expectBson := bson.M{
+		"desc":           "Inner Description",
+		"obj_id":         objID,
+		"name":           "Alice",
+		"nick_name":      "A",
+		"in_name":        "Inner Name",
+		"inner_one.city": "New York",
+		"avatar":         "avatar.png",
+		"NoBsonTag":      "no bson tag",
+		"tm":             main.Tm,
+		"slice_str":      main.SliceStr,
+		"slice_time":     main.SliceTime,
+		"anonymous": bson.M{
+			"name":       main.Anonymous.Name,
+			"in_name":    main.Anonymous.InName,
+			"obj_id":     objID,
+			"desc":       main.Anonymous.InlineName.Desc,
+			"nick_name":  main.Anonymous.InnerAnonymous.NickName,
+			"slice_str":  main.Anonymous.SliceStr,
+			"slice_time": main.Anonymous.SliceTime,
+		},
 	}
 
-	// 验证匿名字段
-	anonymous, ok := result["anonymous"].(bson.M)
-	if !ok {
-		t.Errorf("Unexpected values for anonymous fields")
-	}
-	if anonymous["obj_id"] != objID || anonymous["desc"] != main.Anonymous.InlineName.Desc || anonymous["nick_name"] != main.Anonymous.InnerAnonymous.NickName {
-		t.Errorf("Unexpected values for anonymous fields")
+	for k, v := range expectBson {
+
+		if vb, ok := v.(bson.M); ok {
+			for ik, iv := range vb {
+				vvv, ok := result[k+"."+ik]
+				if !ok {
+					t.Errorf("%s %s 值不存在", k, ik)
+					continue
+				}
+				same := cmp.Equal(iv, vvv)
+				if !same {
+					t.Errorf("%s %s 的值%v 与预期%v不一致", k, ik, iv, vvv)
+				}
+			}
+			continue
+		}
+
+		vv, ok := result[k]
+		if !ok {
+			t.Errorf("%s 值不存在", k)
+			continue
+		}
+
+		same := cmp.Equal(v, vv)
+		if !same {
+			t.Errorf("%s 的值%v 与预期%v不一致", k, v, vv)
+		}
+
 	}
 
 }
 
 func TestDiffToBsonM(t *testing.T) {
-	converter := &StructConverter{}
+	converter := NewStructConverter()
 	converter.AddCustomIsZero(new(primitive.ObjectID), func(v reflect.Value, field reflect.StructField) bool {
 		return v.Interface() == primitive.NilObjectID
 	})
 
 	objID := primitive.NewObjectID()
+
 	mainOriginal := MainTypes{
 		Inline: Inline{
 			Desc:  "original description",
@@ -150,8 +185,10 @@ func TestDiffToBsonM(t *testing.T) {
 		},
 		Avatar:    "original_avatar.png",
 		NoBsonTag: "original no bson tag",
+		Tm:        time.Now(),
+		SliceStr:  []string{"111"},
+		SliceTime: []time.Time{time.Now()},
 	}
-
 	mainCurrent := MainTypes{
 		Inline: Inline{
 			Desc:  "current description",
@@ -172,6 +209,23 @@ func TestDiffToBsonM(t *testing.T) {
 		NoBsonTag: "current no bson tag",
 	}
 
+	var sliceTime []time.Time
+	var sliceStr []string
+
+	expectBson := bson.M{
+		// 因为obj_id一致 所以不会出现
+		//"obj_id":objID,
+		"name":           "Current Alice",
+		"desc":           "Current Inner Description",
+		"nick_name":      "CA",
+		"in_name":        "Current Inner Name",
+		"inner_one.city": "Current City",
+		"avatar":         "current_avatar.png",
+		"NoBsonTag":      "current no bson tag",
+		"slice_time":     any(sliceTime),
+		"slice_str":      any(sliceStr),
+	}
+
 	partCurrent := MainTypes{
 		InlineName: InlineName{
 			InName: "part Inner Name",
@@ -184,13 +238,21 @@ func TestDiffToBsonM(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error in DiffToBsonM: %v", err)
 	}
-	if result["desc"] != "Current Inner Description" || result["in_name"] != "Current Inner Name" {
-		t.Errorf("Unexpected values for diff fields")
+
+	for k, v := range expectBson {
+		vv := result[k]
+		same := cmp.Equal(v, vv)
+		if !same {
+			t.Errorf("%s 的值%v 与预期%v不一致", k, v, vv)
+		}
 	}
 
 	result, err = converter.DiffToBsonM(mainOriginal, partCurrent)
 	if err != nil {
 		t.Errorf("Error in DiffToBsonM: %v", err)
+	}
+	if result["in_name"] != partCurrent.InlineName.InName || result["desc"] != partCurrent.InlineName.Desc {
+		t.Errorf("内联修改获取失败")
 	}
 
 	// 测试内联结构体的冻结字段

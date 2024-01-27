34c491103a52411f3fe9ebe26b4637ddcb4b6b75
1 <1@qq.com>
2024-01-27 11:49:55 +0800

    update `ut` iris err

--- ut/handler_iris.go
+++ ut/handler_iris.go
@@ -1,15 +1,27 @@
 package ut
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+	"net/http"
+)
 
-func IrisErrReturn(ctx iris.Context, msg string, err ...error) {
-	ctx.StatusCode(iris.StatusBadRequest)
-	var content = msg
-	if len(content) < 1 {
-		if len(err) >= 1 {
-			content = err[0].Error()
-		}
+func IrisErrReturn(ctx iris.Context, input any, statusCode int, businessCode int) {
+	ctx.StatusCode(statusCode)
+	var content any = ""
+	switch input.(type) {
+	case string:
+		content = input.(string)
+		break
+	case error:
+		content = input.(error).Error()
+		break
+	default:
+		content = input
 	}
-	_ = ctx.JSON(iris.Map{"detail": content, "code": 400})
+	_ = ctx.JSON(iris.Map{"detail": content, "code": businessCode})
 	return
+}
+
+func IrisErr(ctx iris.Context, input any) {
+	IrisErrReturn(ctx, input, http.StatusBadRequest, http.StatusBadRequest)
 }
